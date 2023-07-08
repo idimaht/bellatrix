cfc5ca83209cf62e0a2c7f00ae6fd58c1c2127ef
idimaht <idimaht.x@gmail.com>
2023-07-08 14:40:02 +0700

    feat: create user api

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { registerAs } from '@nestjs/config';
+import { config } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+config({ path: '.env' });
+
+console.log('typeorm config file');
+export const dataSourceOptions: DataSourceOptions = {
+  type: process.env.DB_CONNECTION as any,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: false,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/database/migrations/*.{ts,js}'],
+};
+
+export default registerAs('typeorm', () => dataSourceOptions);
+export const connectionSource = new DataSource(dataSourceOptions);
