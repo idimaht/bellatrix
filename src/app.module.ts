33c630ae84935a21c451d76be6e947857bbf1326
idimaht <idimaht.x@gmail.com>
2023-07-08 14:40:02 +0700

    feat: create user api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,34 @@
+import typeorm from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpModule } from './app/http/http.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
     HttpModule,
-    TypeOrmModule.forRoot({
-      type: process.env.DB_CONNECTION as any,
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// type: process.env.DB_CONNECTION as any,
+// host: process.env.DB_HOST,
+// port: +process.env.DB_PORT,
+// username: process.env.DB_USERNAME,
+// password: process.env.DB_PASSWORD,
+// database: process.env.DB_DATABASE,
+// entities: [__dirname + '/**/*.entity{.ts,.js}'],
+// migrations: [__dirname + 'migrations/*{.ts,.js}'],
