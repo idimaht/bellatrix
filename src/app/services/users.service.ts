12664bb06b8d6390f50c7bd1c98650f88c210f79
idimaht <idimaht.x@gmail.com>
2023-07-10 17:55:47 +0700

    feat: add pagination of user

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,34 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateDto } from '../http/dtos/users/create.dto';
+import {
+  IPaginationOptions,
+  Pagination,
+  paginate,
+} from 'nestjs-typeorm-paginate';
+import { Request } from 'express';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(UserEntity)
-    private usersRepository: Repository<UserEntity>,
+    private userRepository: Repository<UserEntity>,
   ) {}
 
-  async findAll(): Promise<UserEntity[]> {
-    return await this.usersRepository.find();
+  async paginate(
+    req: Request,
+    options: IPaginationOptions,
+  ): Promise<Pagination<UserEntity>> {
+    const builder = this.userRepository.createQueryBuilder('users');
+
+    // order by ASC | DESC
+    if (req.query.orderBy) {
+      builder.orderBy('users.id', `${req.query.orderBy}` as any);
+    }
+
+    // filter
+    if (req.query.search)
+      builder.where('users.name LIKE :search', {
+        search: `%${req.query.search}%`,
+      });
+
+    return await paginate<UserEntity>(builder, options);
   }
 
-  async findOne(id: number): Promise<UserEntity> {
-    return await this.usersRepository.findOne({ where: { id } });
+  async findById(id: number): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return user;
   }
 
   // change use dto in this
   async create(dto: CreateDto): Promise<UserEntity> {
-    const user = this.usersRepository.create(dto);
-    console.log('service: ', dto);
-    return await this.usersRepository.save(user);
+    try {
+      const user = this.userRepository.create(dto);
+      return await this.userRepository.save(user);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async delete(id: number): Promise<any> {
-    const user = +this.findOne(id);
+    try {
+      console.log('delete', id);
 
-    await this.usersRepository.softDelete(user);
+      const user = await this.findById(id);
+      console.log('delete', user);
+
+      const deleteUser = this.userRepository.softDelete(user);
+      console.log('delete', deleteUser);
+
+      return deleteUser;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
