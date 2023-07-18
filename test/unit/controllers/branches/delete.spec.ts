b827f3689f4790bd48aa267e671954ea3e28921f
idimaht <idimaht.x@gmail.com>
2023-07-18 17:50:29 +0700

    feat: add paginate branch

---
+++ test/unit/controllers/branches/delete.spec.ts
@@ -0,0 +1,56 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { BranchesService } from '../../../../src/app/services/branches.service';
+import { BranchEntity } from '../../../../src/app/entities/branch.entity';
+import { plainToInstance } from 'class-transformer';
+import { DeleteController } from '../../../../src/app/backend/controllers/branches/delete.controller';
+
+describe('branches controller', () => {
+  let controller: DeleteController;
+  let service: Partial<BranchesService>;
+
+  beforeEach(async () => {
+    service = {
+      delete: jest.fn(),
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: BranchesService,
+          useValue: service,
+        },
+      ],
+
+      controllers: [DeleteController],
+    }).compile();
+
+    controller = module.get<DeleteController>(DeleteController);
+  });
+
+  it('should be defined', async () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('delete', () => {
+    it('should response 200 ok', async () => {
+      const showBranch = await controller.delete(1);
+
+      expect(showBranch.status.statusCode).toEqual(200);
+      expect(showBranch.status.message).toBe('OK');
+    });
+  });
+
+  describe('delete', () => {
+    it('should throw an exception', async () => {
+      jest.spyOn(service, 'delete').mockRejectedValue(new Error('error'));
+
+      try {
+        await controller.delete(1);
+
+        expect(controller.delete).toHaveBeenCalled();
+      } catch (error) {
+        return;
+      }
+    });
+  });
+});
